eb5b5615e4f15e2572c3faeddb053d84f06112e2
Davichobits <davidruizdeveloper@gmail.com>
2025-06-19 18:03:48 -0500

    feat: enhance Home component with background image and improved styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+
 export default function Home() {
   return (
-    <header className="text-center bg-Dark-Gray">
-      <p className="opacity-50 uppercase">New product</p>
+    <header className="text-center bg-Dark-Gray bg-[url(/assets/home/mobile/image-header.jpg)] bg-cover pt-[108px] bg-position-[center_top_-4rem] h-[510px]">
+      
+      <p className="opacity-50 uppercase mb-4">New product</p>
       <h1 className="uppercase text-4xl font-bold mb-6">XX99 Mark II HeadphoneS</h1>
-      <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-      <button>See product</button>
+      <p className="text-[.9375rem] w-80 m-auto mb-[28px]">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+      <button className="bg-Orange w-40 h-12 uppercase cursor-pointer hover:bg-Soft-Orange">See product</button>
     </header>
   );
 }
